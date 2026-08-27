e829ba25321a5ffacf356256c3d8de8b94664b30
btailor26 <bhavtee@gmail.com>
2026-08-28 00:58:18 +0100

    fix: install FBM parcel autosave route

--- services/governed_mcf_compat.py
+++ services/governed_mcf_compat.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 from mcf_service import MCFService
 from services.governed_mcf_execution import refresh_mcf_status, submit_mcf_order
 import services.fbm_packlink_draft_alignment  # noqa: F401
+import services.fbm_operational_autosave  # noqa: F401
 
 
 def _submit(self, mcf_order):
