2a736721a7f4403a5e81faf41a2a62137182809e
btailor26 <bhavtee@gmail.com>
2026-08-28 14:55:12 +0100

    fix: install unified FBM shipping alignment

--- services/governed_mcf_compat.py
+++ services/governed_mcf_compat.py
@@ -10,7 +10,9 @@ from services.governed_mcf_execution import refresh_mcf_status, submit_mcf_order
 import services.fbm_packlink_draft_alignment  # noqa: F401
 import services.fbm_operational_autosave  # noqa: F401
 import services.fbm_marketplace_order_update_alignment  # noqa: F401
-import services.fbm_live_feed_alignment  # noqa: F401
+import services.fbm_unified_shipping_alignment  # noqa: F401
+import services.fbm_packlink_location_fallback  # noqa: F401
+import services.fbm_page_performance_alignment  # noqa: F401
 
 
 def _submit(self, mcf_order):
