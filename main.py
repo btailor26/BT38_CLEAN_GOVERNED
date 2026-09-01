39d4dea6e8240c275e66cc67bf2ea5f39373326e
btailor26 <bhavtee@gmail.com>
2026-09-01 16:22:34 +0100

    feat: activate SDS FBM read alignment

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ from services.governed_shipping_spend_reporting import (
 from services.governed_seller_delivery_config import (
     install_governed_seller_delivery_config,
 )
+from services.governed_sds_fbm_read_alignment import (
+    install_governed_sds_fbm_read_alignment,
+)
 
 install_governed_notification_read_alignment(app)
 install_product_linking_unlink_alignment(app)
@@ -36,6 +39,7 @@ install_governed_fbm_db_authority_alignment()
 install_governed_shipping_spend_alignment(app)
 install_governed_shipping_spend_reporting(app)
 install_governed_seller_delivery_config(app)
+install_governed_sds_fbm_read_alignment()
 
 from services.governed_ebay_notification_challenge import (
     install_ebay_notification_challenge_handler,
